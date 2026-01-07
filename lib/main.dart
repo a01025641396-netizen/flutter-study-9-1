cf35af2e0d738ad1f7587e855814f0f3099af859
최승현 <a01025641396@gmail.com>
2026-01-07 10:30:19 +0900

    feat:main수정

--- lib/main.dart
+++ lib/main.dart
@@ -1,108 +1,144 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_practice_1/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(MyApp());
+  await Firebase.initializeApp(
+    // name:
+    //     "NOT_DEFAULT", // prevent `A Firebase App named "[DEFAULT]" already exists`
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: HomePage());
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
   }
 }
 
-/// - ListView
-///     - ClipRRect : 자녀위젯 그냥 잘라줌
-///         - Image
-///     - Row (제목행 이라고 부르자!)(TODO 간격 구현하면서 고민!)
-///         - Text
-///         - Text
-///     - Row
-///         - Column (큰앨범 이라고 부르자!)
-///             - ClipRRect
-///                 - Image
-///             - Text
-///             - Text
-///         - 큰앨범
-///         - 큰앨범
-///     - 제목행
-///     - Row (작은앨범 이라고 부르자!)
-///         - ClipRRect
-///             - Image
-///         - Column
-///             - Text
-///             - Text
-///         - Icon
-///     - 작은앨범
-///     - 작은앨범
-class HomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // 여기서 matzip 폴더(컬렉션)에 맛집 파일(문서) 하나 추가할거임
+      // 1. 폴더 찾아갈 수 있는 행위 -> 컬렉션 참조 만들기
+      final colRef = FirebaseFirestore.instance.collection('matzip');
+
+      // 2. 위에서 만든 컬렉션 참조로 문서 만들기
+      Map<String, dynamic> data = {
+        '상호명': '이재모피자',
+        '주소': '부산광역시 부산진구',
+        '맛': '기대보다 별로',
+      };
+      colRef.add(data);
+
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        actions: [
-          Container(
-            width: 40,
-            height: 40,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(100),
-              color: Colors.black,
-              //
-            ),
-            child: Icon(Icons.search, color: Colors.white),
-          ),
-          SizedBox(width: 16),
-        ],
-        // title 속성은 안드로이드에서는 왼쪽 기본
-        // 아이폰에서는 가운데 기본
-        // centerTitle false 주게되면은 왼쪽으로 가지만
-        // title에 Column 을 배치하면 Column 크기가 앱바 영역전체를
-        // 차지하기 때문에 무용지물
-        // 이럴땐 Column의 crossAxisAlignment 속성으로 조정
-        // Column과 Row의 crossAxisAlignment 기본값은 가운데!
-        title: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+        // TRY THIS: Try changing the color here to a specific color (to
+        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+        // change color while the other colors stay the same.
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          //
+          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
+          // action in the IDE, or press "p" in the console), to see the
+          // wireframe for each widget.
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            const Text('You have pushed the button this many times:'),
             Text(
-              "Music",
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-                //
-              ),
-            ),
-            Row(
-              children: [
-                Container(
-                  width: 16,
-                  height: 5,
-                  decoration: BoxDecoration(
-                    color: Colors.black,
-                    borderRadius: BorderRadius.circular(100),
-                    //
-                  ),
-                ),
-                SizedBox(width: 3),
-                Container(
-                  width: 5,
-                  height: 5,
-                  decoration: BoxDecoration(
-                    color: Colors.grey,
-                    borderRadius: BorderRadius.circular(100),
-                    //
-                  ),
-                ),
-                //
-              ],
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
             ),
           ],
         ),
       ),
-      //
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
     );
   }
 }
