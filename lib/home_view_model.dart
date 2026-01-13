ba7ddc8cde60db98e4e0d858627b930c583e620e
최승현 <a01025641396@gmail.com>
2026-01-13 10:22:37 +0900

    WIP:워밍업

--- lib/home_view_model.dart
+++ lib/home_view_model.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_practice_1/matzip.dart';
+import 'package:flutter_practice_1/matzip_repository.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class HomeState {
@@ -11,7 +12,17 @@ class HomeViewModel extends Notifier<HomeState> {
   @override
   HomeState build() {
     print('뷰모델 bulid 호출됨');
+    getData();
+
     return HomeState(matzippList: []);
+  }
+
+  void getData() async {
+    print("");
+    final repo = MatzipRepository();
+    final matzipList = await repo.getAllMatzip();
+    // 여기서 Firestore 데이터 가져와서 Matzip 리스트로 변환한뒤
+    // 상태 업데이트!
   }
 }
 
